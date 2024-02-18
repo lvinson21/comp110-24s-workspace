bf3e420ffe098b7a37c7af1e970c346c46ee3094
Liza Vinson <lvinson@ad.unc.edu>
2024-02-17 22:01:59 -0500

    updated excersize 3

--- exercises/ex03_functional_battleship.py
+++ exercises/ex03_functional_battleship.py
@@ -1,7 +1,9 @@
-"""Functional Battleship Excersize"""
+"""Functional Battleship Excersize."""
+
 
 __author__ = "730404818"
 import random
+
 
 def input_guess(num: int, word: str) -> int:
     """This function allows you to add in your guess as a user and sets parameters for each guess."""
@@ -19,7 +21,7 @@ def input_guess(num: int, word: str) -> int:
 
 
 def print_grid(grid_size: int, row_guess: int, column_guess: int, users_guess: bool) -> None:
-    """This function prints out the grid for if you hit or miss"""
+    """This function prints out the grid for if you hit or miss."""
     BLUE_BOX: str = "\U0001F7E6"
     RED_BOX: str = "\U0001F7E5"
     WHITE_BOX: str = "\U00002B1C"
@@ -35,7 +37,7 @@ def print_grid(grid_size: int, row_guess: int, column_guess: int, users_guess: b
         emoji_row: str = ''
         column_counter: int = 1
         if row_guess == row_counter:
-            """Then go down the column on that row 1 by 1 and evaluate to see where to addd colored box"""
+            """Then go down the column on that row 1 by 1 and evaluate to see where to addd colored box."""
             while column_counter <= grid_size:
                 if column_counter == column_guess:
                     emoji_row += result_box
@@ -43,21 +45,22 @@ def print_grid(grid_size: int, row_guess: int, column_guess: int, users_guess: b
                     emoji_row += BLUE_BOX
                 column_counter += 1
         else:
-            """While row guess does not equal counter then just add 4 blue boxes to that row"""
+            """While row guess does not equal counter then just add 4 blue boxes to that row."""
             while column_counter <= grid_size:
                 emoji_row += BLUE_BOX
                 column_counter += 1
-        """Print out the row, add 1 to the row counter, then restart until all 4 rows are complete"""
+        """Print out the row, add 1 to the row counter, then restart until all 4 rows are complete."""
         print(emoji_row)
         row_counter += 1
 
 
 def correct_guess(secret_row: int, secret_column: int, row_guess: int, column_guess: int) -> bool:
-    """Tells you if you guessed correctly or not"""
+    """Tells you if you guessed correctly or not."""
     return secret_row == row_guess and secret_column == column_guess
 
+
 def main(grid_size: int, secret_row: int, secret_column: int) -> None:
-    """implements all other function to allow your game to run"""
+    """Implements all other function to allow your game to run."""
     num_turns: int = 5
     turn_counter: int = 1
     game_won: bool = False
@@ -69,7 +72,7 @@ def main(grid_size: int, secret_row: int, secret_column: int) -> None:
         users_guess = correct_guess(secret_row, secret_column, row_guess, column_guess)
         print_grid(grid_size, row_guess, column_guess, users_guess)
 
-        if users_guess == True:
+        if users_guess:
             game_won = True
             print("Hit!")
             print(f"You won {turn_counter}/5 turns!")
@@ -79,14 +82,7 @@ def main(grid_size: int, secret_row: int, secret_column: int) -> None:
     if turn_counter > num_turns:
         print("X/5 - Better luck next time!")
 
+
 if __name__ == "__main__":
     grid_size: int = random.randint(3, 5)
     main(grid_size, random.randint(1, grid_size), random.randint(1, grid_size))
-
-
-
-
-
-
-
-
