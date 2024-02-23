550d816123cd90e1b56591705b32568c2de92390
Liza Vinson <lvinson@ad.unc.edu>
2024-02-23 12:15:15 -0500

    updated ex04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -5,6 +5,9 @@ __author__ = "730404818"
 
 
 def all(my_list: list[int], num: int) -> bool:
+    """Shows whether the list values are equal to num."""
+    if len(my_list) == 0:
+        return False
     for elem in my_list:
         if elem != num:
             return False
@@ -12,6 +15,7 @@ def all(my_list: list[int], num: int) -> bool:
 
 
 def max(input: list[int]) -> int:
+    """Gives the largest number in the list."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
     max_value = input[0]
@@ -22,6 +26,7 @@ def max(input: list[int]) -> int:
 
 
 def is_equal(list_one: list[int], list_two: list[int]) -> bool:
+    """Tells you if one list has the same values as the other one."""
     if len(list_one) != len(list_two):
         return False
     
@@ -36,6 +41,7 @@ def is_equal(list_one: list[int], list_two: list[int]) -> bool:
 
 
 def extend(list_one: list[int], list_two: list[int]) -> None:
+    """Add the values from one list to the end of the other list."""
     index = 0
     while index < len(list_two):
         list_one.append(list_two[index])
