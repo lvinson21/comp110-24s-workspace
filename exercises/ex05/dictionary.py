c72030f31ba56b2f81f9099472fb22004fa894a7
Liza Vinson <lvinson@ad.unc.edu>
2024-03-06 15:39:01 -0500

    uopdated ex05

--- exercises/ex05/dictionary.py
+++ exercises/ex05/dictionary.py
@@ -5,23 +5,22 @@ __author__ = "730404818"
 
 
 def invert(given_dict: dict[str, str]) -> dict[str, str]:
-    """Inverts the keys and values in the dictionary"""
+    """Inverts the keys and values in the dictionary."""
     inverted_dict: dict[str, str] = {}
     for key in given_dict:
-        if given_dict(key) in inverted_dict:
+        if given_dict[key] in inverted_dict:
             raise KeyError("Duplicate value.")
-        inverted_dict[given_dict(key)] = key
+        inverted_dict[given_dict[key]] = key
     return inverted_dict
 
 
 def favorite_color(given_dict: dict[str, str]) -> str:
     """Returns the most popular color."""
-    most_popular_color = None
+    most_popular_color: str = ""
     max_count = 0
     for name in given_dict:
         color = given_dict[name]
-        count = 0
-
+        count: int = 0
         for other_name in given_dict:
             if given_dict[other_name] == color:
                 count += 1
@@ -32,7 +31,8 @@ def favorite_color(given_dict: dict[str, str]) -> str:
 
 
 def count(numbers: list[str]) -> dict[str, int]:
-    result_dict: dict[str, int] = []
+    """Counts the number of times each word appears."""
+    result_dict: dict[str, int] = {}
     for item in numbers:
         if item in result_dict:
             result_dict[item] += 1
@@ -42,7 +42,8 @@ def count(numbers: list[str]) -> dict[str, int]:
 
 
 def alphabetizer(words: list[str]) -> dict[str, list[str]]:
-    result_dict: dict[str, int] = []
+    """Alphabetizes the words."""
+    result_dict: dict[str, list[str]] = {}
     for word in words:
         first_letter = word[0].lower()
         if first_letter not in result_dict:
@@ -52,8 +53,9 @@ def alphabetizer(words: list[str]) -> dict[str, list[str]]:
 
 
 def update_attendance(students: dict[str, list[str]], day: str, student: str) -> dict[str, list[str]]:
+    """Gives a list of students in attendance each day."""
     if day in students: 
         students[day].append(student)
     else:
         students[day] = [student]
-    return students
+    return students 
